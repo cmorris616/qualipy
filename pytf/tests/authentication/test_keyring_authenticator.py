a598fce9d894a82da60484b1c4e82f4cc3ca7ddb
Hacker Cat <hackercat3211@gmail.com>
2023-07-27 21:25:09 -0400

    Added some tests for the keyring authenticator

--- pytf/tests/authentication/test_keyring_authenticator.py
+++ pytf/tests/authentication/test_keyring_authenticator.py
@@ -5,8 +5,26 @@ from pytf.authentication.keyring_authenticator import KeyringAuthenticator
 
 class TestKeyringAuthenticator:
 
-    # @patch('authentication.keyring_authenticator.keyring')
-    def test_get_username(self):
+    @patch('pytf.authentication.keyring_authenticator.keyring')
+    def test_get_username(self, keyring_mock):
         authenticator = KeyringAuthenticator(system='test')
         authenticator.get_username()
-        # keyring_mock.get_password.assert_called_with('test', 'username')
+        keyring_mock.get_password.assert_called_with('test', 'username')
+
+    @patch('pytf.authentication.keyring_authenticator.keyring')
+    def test_get_password(self, keyring_mock):
+        authenticator = KeyringAuthenticator(system='test')
+        authenticator.get_password()
+        keyring_mock.get_password.assert_called_with('test', 'password')
+
+    @patch('pytf.authentication.keyring_authenticator.keyring')
+    def test_get_api_key(self, keyring_mock):
+        authenticator = KeyringAuthenticator(system='test')
+        authenticator.get_api_key()
+        keyring_mock.get_password.assert_called_with('test', 'api_key')
+
+    @patch('pytf.authentication.keyring_authenticator.keyring')
+    def test_get_certificate(self, keyring_mock):
+        authenticator = KeyringAuthenticator(system='test')
+        authenticator.get_certificate()
+        keyring_mock.get_password.assert_called_with('test', 'certificate')
